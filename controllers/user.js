66c7c6e07cfd9fb7fb9939a1f2812bad196fc9a1
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2023-12-16 13:25:58 +0530

    added login page

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,8 @@
 const User = require("../models/user");
 
+// Adding uuid for user i.e user unique id
+const {v4: uuidv4} = require('uuid')
+
 async function handleUserSignUp(req,res) {
     const {name,email,password} = req.body;
 
@@ -9,7 +12,25 @@ async function handleUserSignUp(req,res) {
         password
     });
 
-    return res.render("Home");
+    return res.redirect("/");
 }
 
-module.exports = handleUserSignUp;
+async function handleUserLogin(req,res) {
+   const {email,password} = req.body;
+
+  const user =  await User.findOne({email,password})
+
+   if(!user)
+     return res.render("login",{
+     error: "Invalid username or password",
+    })
+
+    // if login is successful then these will generate a session id
+    const sessionId = uuidv4();
+   return res.redirect("/");
+}
+
+module.exports = {
+    handleUserSignUp,
+    handleUserLogin
+}
